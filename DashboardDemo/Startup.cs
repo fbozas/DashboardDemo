afb325cd54376c4bccd84a611fdbdb4d1b5f5032
Panos Bozas <fbozas@hotmail.com>
2022-06-29 18:53:59 +0300

    Add project files.

---
+++ DashboardDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DashboardDemo.Startup))]
+namespace DashboardDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
